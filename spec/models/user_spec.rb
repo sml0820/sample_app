4d367e05cf4981a658888496f13e177f6888239c
Sean Lanning <sml0820@gmail.com>
2013-04-05 13:57:38 -0400

    Add some missing admin code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,13 @@ require 'spec_helper'
     	end
     end
 
+    describe "with admin attribute set to 'true'" do
+    	before { @user.toggle!(:admin)}
+
+    	it { should be_admin }
+
+    end
+
 	describe "when name is not present" do
 		before { @user.name = " "}
 		it { should_not be_valid }
